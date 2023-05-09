a9b222a75ae1a49e6bf139e4a281abf7c83ef7ac
Vesna Zaric <4vesnazaric@gmail.com>
2023-05-09 07:06:20 -0700

    second commit errors still need to be fixed

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Hello, "Hello");
+assertEqual("hello", "world");
+assertEqual(42, 42);
+assertEqual(42, 99);
