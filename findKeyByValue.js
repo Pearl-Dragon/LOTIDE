616165454b1750762fd397ea8121a4ce9bf633c6
Vesna Zaric <4vesnazaric@gmail.com>
2023-05-17 02:13:41 -0700

    FindKeyByValue.js final commit

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const findKeyByValue = function(object, value) {
+  for (const key in object) {
+    if (object.hasOwnProperty(key) && object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+
+};
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🆘 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
