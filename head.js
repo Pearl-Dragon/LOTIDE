741066735ff559a12d8ba6aecc6a17bb0ab74bdd
Vesna Zaric <4vesnazaric@gmail.com>
2023-05-29 12:34:15 -0700

    inported assertEqual Fn, cut out the test and moved it to headTest.js

--- head.js
+++ head.js
@@ -1,23 +1,19 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🆘Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+ 
 const head = function(array) {
-  return array [0];// Fn to retrieve the head of an array
+   return array [0];// Fn to retrieve the head of an array
 };
 
-//ssertEqual(head(["apple", 1, 8]), 1);
+assertEqual(head(["apple", 1, 8]), 1);
 
 const words = ["L", "Lighthouse", "Labs"];
-//tail(words);
-assertEqual(words.length[3]);
-assertEqual(words.length, 3);
+// tail(words);
+// assertEqual(words.length[3]);
+// assertEqual(words.length, 3);
 
-// Test cases
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// // Test cases
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
 
+module.exports = head;
