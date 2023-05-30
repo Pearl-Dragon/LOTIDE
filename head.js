9952a261d9e37cc4286bf65f662d943f6ca5d95b
Vesna Zaric <4vesnazaric@gmail.com>
2023-05-30 09:16:24 -0700

    removed unnecessary lines from head.js

--- head.js
+++ head.js
@@ -7,13 +7,6 @@ const head = function(array) {
 assertEqual(head(["apple", 1, 8]), 1);
 
 const words = ["L", "Lighthouse", "Labs"];
-// tail(words);
-// assertEqual(words.length[3]);
-// assertEqual(words.length, 3);
-
-// // Test cases
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
+// tail(words)
 
 module.exports = head;
