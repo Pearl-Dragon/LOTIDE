a06c47211269518cb4af9d8dd7ed18b5081bff4e
Vesna Zaric <4vesnazaric@gmail.com>
2023-05-30 16:59:17 -0700

    test/tailTest.js complete

---
+++ test/tailTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+//pasted the test code from tail.js
+const array = tail(["Hello", "Lighthouse", "Labs", "Bootcamp"]);
+assertEqual(array.length, 3);
+assertEqual(array[0], "Lighthouse");
+assertEqual(array[1], "Labs");
