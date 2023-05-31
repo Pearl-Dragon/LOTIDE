4e466d5c3aa94029ad54731b0f0a349042d90104
Vesna Zaric <4vesnazaric@gmail.com>
2023-05-30 18:49:06 -0700

    dry code complete

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🆘Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays');
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !==arr2.length) {
-    return false;
-  }
-for(let i = 0; i< arr1.length;i++) {
-  if (arr1[i] !== arr2[i]) {
-    return false;
-
-  }
-}
-return true;
-};
 const assertArraysEqual = function(actual, expected) {
   const isEqual = eqArrays(actual, expected);
   if (isEqual) {
@@ -27,20 +9,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
-
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-//assertEqual("Lighthouse Labs", "Bootcamp");
-//assertEqual(1, 1);
-//assertEqual("Hello", "Hello");
-//assertEqual("hello", "world");
-//assertEqual(42, 42);
-
-//eqArrays([1, 2, 3], [1, 2, 3]);
-//eqArrays([1, 2, 3], [3, 2, 1]);
-
-//eqArrays(["1", "2", "3"], ["1", "2", "3"]);
-//eqArrays(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
