d8e149c78742daf0ded4a4a54ed07e01fe8468d8
Vesna Zaric <4vesnazaric@gmail.com>
2023-05-30 18:53:23 -0700

    eqArrays.js Dry code

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🆘Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
