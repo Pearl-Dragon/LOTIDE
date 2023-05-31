be5c0b1c4783775c24b432f5f71e84d8fcc73992
Vesna Zaric <4vesnazaric@gmail.com>
2023-05-30 17:38:00 -0700

    EqArrays.js dry code final

--- eqArrays.js
+++ eqArrays.js
@@ -1,19 +1,23 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    //console.log(`👍Assertion Passed: ${actual} === ${expected}`);
+    console.log(`👍Assertion Passed: ${actual} === ${expected}`);
   } else {
-    //console.log(`🆘Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🆘Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-//assertEqual("Lighthouse Labs", "Bootcamp");
-//assertEqual(1, 1);
-//assertEqual("Hello", "Hello");
-//assertEqual("hello", "world");
-//assertEqual(42, 42);
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
 
-eqArrays([1, 2, 3], [1, 2, 3]);
-eqArrays([1, 2, 3], [3, 2, 1]);
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]);
-eqArrays(["1", "2", "3"], ["1", "2", 3]);
+  return true;
+};
+
+
+module.exports = eqArrays;
