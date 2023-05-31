ec660fd7f6373a377bdde689567b8978d8d2978e
Vesna Zaric <4vesnazaric@gmail.com>
2023-05-30 17:03:21 -0700

    tal.js dry code done

--- tail.js
+++ tail.js
@@ -1,24 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🆘Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 const tail = function(array) {
   return array.slice(1);
 };
-// const head = function(array) {
-  // return array [0];
-// };
-//assertEqual(tail(["Hello", "Lighthouse", "Labs", "Bootcamp"]), "Hello");
-// assertEqual(tail([3,1,2]), 3);
-// assertEqual(tail(["apple", 1, 8]), 1);
-const array = tail(["Hello", "Lighthouse", "Labs", "Bootcamp"]);
-assertEqual(array.length, 3);
-assertEqual(array[0], "Lighthouse");
-assertEqual(array[1], "Labs");
 
+module.exports = tail;
 
 
 
