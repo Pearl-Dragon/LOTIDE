2a28390bf040bf4250ca4ce98ff7f16629e2bad1
Vesna Zaric <4vesnazaric@gmail.com>
2023-05-30 17:39:17 -0700

    eqArraysTest.js working well

---
+++ test/eqArraysTest.js
@@ -0,0 +1,15 @@
+const eqArrays = require ('../eqArrays');
+const assertEqual = require ('../assertEqual');
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Hello", "Hello");
+assertEqual("hello", "world");
+assertEqual(42, 42);
+
+eqArrays([1, 2, 3], [1, 2, 3]);
+eqArrays([1, 2, 3], [3, 2, 1]);
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]);
+eqArrays(["1", "2", "3"], ["1", "2", 3]);
