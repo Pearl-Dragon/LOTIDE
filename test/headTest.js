1627984d4d0bc6188e2ef89ca460d8e0710b0c8d
Vesna Zaric <4vesnazaric@gmail.com>
2023-05-30 21:44:22 -0700

    implemented mocha framework with chai functions and removed previous assertEqual function

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,11 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-const words = ["L", "Lighthouse", "Labs"];
-assertEqual(words.length[3]);
-assertEqual(words.length, 3);
-
-// Test cases
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+ });
